d8fb99b6611fa72773f53139becef543fd9e8835
Phittinanphu <64110105@kmitl.ac.th>
2024-05-31 15:09:40 +0700

    hula

--- src/app.py
+++ src/app.py
@@ -9,7 +9,6 @@ load_dotenv()
 # Configure Google API for audio summarization
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=gemini_api_key)
-#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/phitt/OneDrive/Documents/GitHub/GeminiAPI_SummarizeMeeting/gemini-api-424003-6ac26a951d8e.json"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gemini-api-424003-6ac26a951d8e.json"
 
 def summarize_audio(audio_file_path):
